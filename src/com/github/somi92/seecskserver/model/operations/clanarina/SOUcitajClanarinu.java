91737f7f981c7a7e1e6fff7b8d316864c3b82996
Milos Stojanovic <milos92s@yahoo.com>
2015-06-23 19:01:19 +0200

    Fixed fee loading so.

--- src/com/github/somi92/seecskserver/model/operations/clanarina/SOUcitajClanarinu.java
+++ src/com/github/somi92/seecskserver/model/operations/clanarina/SOUcitajClanarinu.java
@@ -6,12 +6,15 @@
 package com.github.somi92.seecskserver.model.operations.clanarina;
 
 import com.github.somi92.seecskcommon.domain.Clanarina;
+import com.github.somi92.seecskcommon.domain.Uplata;
 import com.github.somi92.seecskcommon.util.Ref;
 import com.github.somi92.seecskserver.model.exceptions.so.PreduslovException;
 import com.github.somi92.seecskserver.model.exceptions.so.SOException;
 import com.github.somi92.seecskserver.model.exceptions.so.ValidacijaException;
 import com.github.somi92.seecskserver.model.operations.ApstraktnaSistemskaOperacija;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
@@ -42,7 +45,14 @@ public class SOUcitajClanarinu extends ApstraktnaSistemskaOperacija {
     @Override
     protected void izvrsiDBTransakciju() throws SOException {
         try {
-            clanarina.set(dbbroker.loadEntity(clanarina.get(), false));
+            Clanarina c = dbbroker.loadEntity(clanarina.get(), false);
+            List<String> kriterijumPretrage = new ArrayList<>();
+            kriterijumPretrage.add("idClanarina="+c.getIdClanarina());
+            Uplata u = new Uplata();
+            u.setClanarina(c);
+            List<Uplata> uplate = dbbroker.loadEntities(u, kriterijumPretrage, false);
+            c.setUplate(uplate);
+            clanarina.set(c);
         } catch (SQLException ex) {
             ex.printStackTrace();
             System.out.println("Greska -> "+this.getClass().getName()+": "+ex.getMessage());
